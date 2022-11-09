b6005021a767967a2d55b3ac0ae9f06f3753b954
SERDAR DEMIRSOY <serdardemirsoy@gmail.com>
2022-11-09 20:52:47 +0000

    VehiclesServicesLogs functionality updated

--- src/test/java/com/cydeo/runners/CukesRunner.java
+++ src/test/java/com/cydeo/runners/CukesRunner.java
@@ -6,7 +6,8 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        plugin = "html:target/cucumber-report.html",// determines what type of report we want to generate with our project,and also where we want to store
+        plugin = {"html:target/cucumber-report.html",// determines what type of report we want to generate with our project,and also where we want to store
+                "json:target/cucumber.json"},
         features = "src/test/resources/features",// we provide the path of features directory and let our project know where to find all of the feature files
         glue = "com/cydeo/step_definitions",// glue gives path to the package of the step_definitions
         dryRun = false,
