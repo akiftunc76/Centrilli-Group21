1eb04f9bc95b4035040d8c4c07436b34f6d8d916
elifbasbug <elifbilir@gmail.com>
2022-11-09 01:43:33 +0000

    vehicle costs

---
+++ src/test/java/com/cydeo/step_definitions/Vehicles_StepDefinitions.java
@@ -0,0 +1,194 @@
+package com.cydeo.step_definitions;
+
+import com.cydeo.pages.VehicleCostsPage;
+import com.cydeo.utilities.BrowserUtils;
+import com.cydeo.utilities.ConfigurationReader;
+import com.cydeo.utilities.Driver;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class Vehicles_StepDefinitions {
+
+    VehicleCostsPage vehicleCostsPage = new VehicleCostsPage();
+
+    @Given(": user is already logged in and on main page")
+    public void userIsAlreadyLoggedInAndOnMainPage() {
+
+        // Getting our login method to log in to web table app
+        Driver.getDriver().get(ConfigurationReader.getProperty("cent-url"));
+
+        //Calling our login method to log in to web table app
+        vehicleCostsPage.login();
+        BrowserUtils.waitFor(2);
+    }
+
+    @When("user clicks on more button")
+    public void userClicksOnMoreButton() {
+        vehicleCostsPage.moreButton.click();
+        BrowserUtils.waitFor(2);
+
+    }
+
+    @And("user clicks fleet button")
+    public void userClicksFleetButton() {
+
+        if (vehicleCostsPage.fleetButton.isDisplayed()){
+            vehicleCostsPage.fleetButton.click();
+        }else {
+            vehicleCostsPage.moreButton.click();
+            vehicleCostsPage.fleetButton.click();
+        }
+
+        BrowserUtils.waitFor(2);
+    }
+
+    @And("user clicks Vehicle Costs button")
+    public void userClicksVehicleCostsButton() {
+        vehicleCostsPage.vehicleCostsButton.click();
+    }
+
+    @And("user clicks create button")
+    public void userClicksCreateButton() {
+        vehicleCostsPage.createButton.click();
+        BrowserUtils.waitFor(1);
+
+    }
+
+    @And("user sees the title New")
+    public void userSeesTheTitleNew() {
+        String expectedTitle="New";
+        String actualTitle= vehicleCostsPage.title.getText();
+        Assert.assertEquals(expectedTitle,actualTitle);
+    }
+    @And("user selects a vehicle")
+    public void userSelectsAVehicle() {
+        vehicleCostsPage.vehicleDropdown.click();
+        BrowserUtils.waitFor(1);
+        WebElement audi= Driver.getDriver().findElement(By.linkText("Audi/A1/"));
+        audi.click();
+
+    }
+
+
+    @And("user selects a type")
+    public void userSelectsAType() {
+        vehicleCostsPage.typeDropdown.click();
+        BrowserUtils.waitFor(1);
+        WebElement snowTires= Driver.getDriver().findElement(By.linkText("Snow tires"));
+        snowTires.click();
+        BrowserUtils.waitFor(1);
+    }
+
+    @And("user selects a date")
+    public void userSelectsADate() {
+        vehicleCostsPage.dateInput.sendKeys("11/02/2022");
+
+    }
+
+    @And("user enters total price")
+    public void userEntersTotalPrice() {
+
+        vehicleCostsPage.inputTotalPrice.clear();
+        vehicleCostsPage.inputTotalPrice.sendKeys("100.00");
+        BrowserUtils.waitFor(1);
+    }
+
+    @And("user enters cost description")
+    public void userEntersCostDescription() {
+        vehicleCostsPage.inputCostDescription.sendKeys("snow tires");
+        BrowserUtils.waitFor(1);
+    }
+
+
+
+    @And("user clicks on save button")
+    public void userClicksOnSaveButton() {
+        vehicleCostsPage.saveButton.click();
+        BrowserUtils.waitFor(2);
+    }
+
+
+
+    @Then("title is changed to {string} and new table is created")
+    public void titleIsChangedToFrom(String vehicle) {
+
+        Assert.assertTrue(vehicleCostsPage.title.isDisplayed());
+        BrowserUtils.waitFor(2);
+
+    }
+
+    @Then("user sees These fields are invalid error message")
+    public void userSeesTheseFieldsAreInvalidErrorMessage() {
+        Driver.getDriver().getWindowHandles();
+        vehicleCostsPage.warningMessage.isDisplayed();
+        BrowserUtils.waitFor(2);
+        Assert.assertTrue(vehicleCostsPage.warningMessage.isDisplayed());
+
+    }
+
+
+    @And("user clicks on discard button")
+    public void userClicksOnDiscardButton() {
+        vehicleCostsPage.discardButton.click();
+        BrowserUtils.waitFor(2);
+
+
+    }
+
+    @And("user sees warning message")
+    public void userSeesWarningMessage() {
+        vehicleCostsPage.warningMessage.isDisplayed();
+        Assert.assertTrue(vehicleCostsPage.warningMessage.isDisplayed());
+        BrowserUtils.waitFor(2);
+
+
+    }
+
+    @And("user clicks on ok button")
+    public void userClicksOnOkButton() {
+        vehicleCostsPage.okButton.click();
+        BrowserUtils.waitFor(2);
+
+    }
+
+    @Then("user is back to vehicle costs page")
+    public void userIsBackToVehicleCostsPage() {
+        vehicleCostsPage.title.isDisplayed();
+        BrowserUtils.waitFor(2);
+
+    }
+
+    @Then("user sees the title is vehicle costs")
+    public void userSeesTheTitleIsVehicleCosts() {
+        Assert.assertTrue(vehicleCostsPage.title.isDisplayed());
+        BrowserUtils.waitFor(2);
+
+    }
+
+    @Then("title is changed to {string} - Odoo")
+    public void titleIsChangedToOdoo(String string) {
+        String title = Driver.getDriver().getTitle();
+        System.out.println("title = " + title);
+
+        BrowserUtils.sleep(2);
+        Assert.assertTrue(title.contains(string));
+        BrowserUtils.sleep(2);
+
+    }
+
+
+    @Then("user sees Attachments and Action buttons")
+    public void userSeesAttachmentSAndActionButtons() {
+        vehicleCostsPage.attachmentButton.isDisplayed();
+        vehicleCostsPage.actionButton.isDisplayed();
+        Assert.assertTrue(vehicleCostsPage.attachmentButton.isDisplayed());
+        Assert.assertTrue(vehicleCostsPage.actionButton.isDisplayed());
+    }
+
+
+}
---
+++ src/test/resources/features/vehicleCosts.feature
@@ -0,0 +1,96 @@
+
+Feature: Create vehicle costs
+
+
+  Scenario: create a vehicle cost
+    Given : user is already logged in and on main page
+    When user clicks on more button
+    And user clicks fleet button
+    And user clicks Vehicle Costs button
+    And user clicks create button
+    And user sees the title New
+    And user selects a vehicle
+    And user selects a type
+    And user selects a date
+    And user enters total price
+    And user enters cost description
+    And user clicks on save button
+    Then title is changed to "Vehicle" and new table is created
+
+
+  Scenario: can not create a vehicle cost
+    Given : user is already logged in and on main page
+    When user clicks on more button
+    And user clicks fleet button
+    And user clicks Vehicle Costs button
+    And user clicks create button
+    And user sees the title New
+    And user clicks on save button
+    Then user sees These fields are invalid error message
+
+
+
+  Scenario: cancel creating a vehicle cost by clicking discard
+    Given : user is already logged in and on main page
+    When user clicks on more button
+    And user clicks fleet button
+    And user clicks Vehicle Costs button
+    And user clicks create button
+    And user sees the title New
+    And user selects a vehicle
+    And user clicks on discard button
+    And user sees warning message
+    And user clicks on ok button
+    Then user is back to vehicle costs page
+
+
+  Scenario: cancel creating a vehicle cost by clicking discard
+    Given : user is already logged in and on main page
+    When user clicks on more button
+    And user clicks fleet button
+    And user clicks Vehicle Costs button
+    And user clicks create button
+    And user sees the title New
+    And user clicks on discard button
+    And user is back to vehicle costs page
+    Then user sees the title is vehicle costs
+
+
+  Scenario: title is changed from new to "vehicle"
+    Given : user is already logged in and on main page
+    When user clicks on more button
+    And user clicks fleet button
+    And user clicks Vehicle Costs button
+    And user clicks create button
+    And user sees the title New
+    And user selects a vehicle
+    And user selects a type
+    And user selects a date
+    And user enters total price
+    And user enters cost description
+    And user clicks on save button
+    Then title is changed to "Audi/A1/" - Odoo
+
+  @wip
+  Scenario: User should see Attachment(s) and Action buttons
+    Given : user is already logged in and on main page
+    When user clicks on more button
+    And user clicks fleet button
+    And user clicks Vehicle Costs button
+    And user clicks create button
+    And user sees the title New
+    And user selects a vehicle
+    And user selects a type
+    And user selects a date
+    And user enters total price
+    And user enters cost description
+    And user clicks on save button
+    Then user sees Attachments and Action buttons
+
+
+
+
+
+
+
+
